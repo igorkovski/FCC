32669a2f003fe85a18d7f8dd04e753ce88c3007b
Igor Tchounkovskii <tchounkovskii@gmail.com>
2015-09-14 23:47:11 -0400

    added some comments to titlecase.js

--- Basic Algorithm Scripting/titleCase.js
+++ Basic Algorithm Scripting/titleCase.js
@@ -1,7 +1,14 @@
+  /* The trick here is to make sure that we first lowercase the entire string
+   before we loop through it, then split it into an array.  
+   If we don't do that, then a string such as: "I'M A LITTLE TEA POT" all caps won't work 
+   given how this function runs. You're obviously free to craft your own.
+   Given that I'm still learning, I'm sure there are ways to "refractor" this code.
+   But this was my first try at creating solutions to these little "String" problems.
+  */
+
 function titleCase(str) {
-  var strArr = str.toLowerCase().split(" ");// The trick here is to make sure that we first lowercase the entire string
-  											// then split it into an array.  If we don't do that, then a string such as:
-  											// "I'M A LITTLE TEA POT" which is all caps won't work given how this function runs
+  var strArr = str.toLowerCase().split(" ");
+
   for (var i = 0; i < strArr.length; i++) {
     
   var k = strArr[i].charAt(0).toUpperCase();
